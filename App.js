9505a176e2b2b74b3662bdcd2190683328d5b1a7
Thays Ribeiro <thaysbribeiro@gmail.com>
2021-10-07 20:24:55 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/scenes/HomeScreen'
+import QuizScreen from './src/scenes/QuizScreen';
+import EndGameScreen from './src/scenes/EndGameScreen';
+import data from './src/assets/data.json';
+
+/*
+    ============Alunos==================
+    Patrick delfim borges - 0050016717
+    Thays dos Santos Barroso Ribeiro - 0050006852
+*/
+
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+
+  const jsonData = data.results.sort((a, b) => 0.5 - Math.random())
+  
+
+
+  return (
+      <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+    headerShown: false
+  }}>
+           <Stack.Screen name="Home" component={HomeScreen} /> 
+          <Stack.Screen name="StartQuiz" component={QuizScreen} initialParams={{'data':jsonData}} />
+          <Stack.Screen name="EndGame" component={EndGameScreen}  />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  );
+}
+
+
